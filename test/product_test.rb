cdae79c54f10e524703f9c07220fe2a54ef03844
Michael Foy <foymikek@gmail.com>
2020-10-14 13:10:38 -0600

    Add passing test_it_can_get_total_price

--- test/product_test.rb
+++ test/product_test.rb
@@ -10,7 +10,6 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
@@ -31,7 +30,6 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_can_get_total_price
-    skip
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
