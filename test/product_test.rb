dd1c199f65f42557ec3dd12329fad2b7b1dbaa5e
Michael Foy <foymikek@gmail.com>
2020-10-14 13:14:56 -0600

    Add passing iteration 1

--- test/product_test.rb
+++ test/product_test.rb
@@ -36,4 +36,12 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_product_can_be_hoarded
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+    refute product.is_hoarded?
+    product.hoard
+    assert product.is_hoarded?
+  end
 end
