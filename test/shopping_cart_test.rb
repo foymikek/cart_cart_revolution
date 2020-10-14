1ad48289782c9a86e404d285a9e1eace184837a5
Michael Foy <foymikek@gmail.com>
2020-10-14 13:26:52 -0600

    Add passing test_shopping_cart_can_add_products

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -4,12 +4,21 @@ require "./lib/product"
 require "./lib/shopping_cart"
 
 class ShoppingCartTest < Minitest::Test
-  def test_it_has_attributes
+  def test_shopping_cart_has_attributes
     cart = ShoppingCart.new("King Soopers", "30items")
 
     assert_equal 'King Soopers', cart.name
-    # require "pry"; binding.pry
     assert_equal 30, cart.capacity
     assert_equal [], cart.products
+  end
+
+  def test_shopping_cart_can_add_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    assert_equal [product1, product2], cart.products
   end
 end
