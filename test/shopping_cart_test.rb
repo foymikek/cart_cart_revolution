a10128a899fa1bd37655aadb0cf0c3dd4158740a
Michael Foy <foymikek@gmail.com>
2020-10-14 14:01:55 -0600

    Add passing test_cart_can_sort_products_by_quantity

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -86,4 +86,19 @@ class ShoppingCartTest < Minitest::Test
 
     assert_equal 43.33, cart.percentage_occupied
   end
+
+  def test_cart_can_sort_products_by_quantity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+    expected = [product3, product2, product1, product4]
+    
+    assert_equal expected, cart.sorted_products_by_quantity
+  end
 end
